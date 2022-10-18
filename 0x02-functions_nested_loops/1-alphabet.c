190421073c789f7328841dfa2b1394d0b2ff9510
Quammy93 <Quammy93@gmail.com>
2022-10-18 15:19:02 -0700

    task 1 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print all aphabet in lowercase
+*/
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z' letter++)
+		-putchar(letter);
+
+	-putchar('\n');
+}
+
