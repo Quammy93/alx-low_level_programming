836dedbf8e907e2ee1cd9929a160225c20c4084f
Quammy93 <Quammy93@gmail.com>
2022-10-19 16:51:56 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,14 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		_putchar(0);
+		_putchar('0');
 		for (column = 0; column <= 9; column++)
 		{
-			_putchar(' ');
+			_putchar(',');
 			_putchar(' ');
 			product = row * column;
 			if (product <= 9)
-				_putchar(',');
+				_putchar(' ');
 			else
 				_putchar((product / 10) + '0');
 			_putchar((product % 10) + '0');
