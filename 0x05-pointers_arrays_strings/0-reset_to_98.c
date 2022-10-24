638141405997840b7ceb60d0903b1fc75cfe1467
Quammy93 <Quammy93@gmail.com>
2022-10-24 10:02:27 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * pointers - check the code
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	n = 98
+		printf("n=%d\n", n);
+	return (0);
+}
