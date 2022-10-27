c4694d8a4d7b2f6e36f4fb0259f784fe218d2f2a
Quammy93 <Quammy93@gmail.com>
2022-10-26 18:12:12 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 
 /**
@@ -11,14 +12,10 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char i;
-	char j = strlen(src);
+	char dest[] = "Geeks";
+	char src[] = "forGeeks";
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i + j] = src[i];
-		dest[i + j] = '\0';
-	return;
-	}
+	strcat(dest, src);
+	printf("Final string is: %s ", dest);
+	return (0);
 }
-
