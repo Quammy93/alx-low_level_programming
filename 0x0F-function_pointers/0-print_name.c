3bed06254f5155462dfaf1213f715596c20a0fc4
Quammy93 <Quammy93@gmail.com>
2022-11-16 19:38:07 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name -  function that prints a name.
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+
+void print_name_as_is(char *name)
+{
+	printf("%s\n", name);
+}
